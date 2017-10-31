3322020d60c8bb6391e7bb17aff45c2b30510184
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-10-31 21:06:31 +0100

    Ready to stat TDD on PrayerRepository

---
+++ PrayerBookDALTest/Helpers/TestContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using PrayerBookDAL.Context;
+
+namespace PrayerBookDALTest.Helpers
+{
+    public class TestContext
+    {
+        public static PrayerBookContext Context => new PrayerBookContext(new DbContextOptionsBuilder<PrayerBookContext>()
+            .UseInMemoryDatabase($"{Guid.NewGuid()}")
+            .Options); 
+    }
+}
---
+++ PrayerBookDALTest/PrayerRepositoryShould.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+
+namespace PrayerBookDALTest
+{
+    public class PrayerRepositoryShould : IRepositoryTest
+    {
+        [Fact]
+        public void CreateOne()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void NotCreateOneParseNull()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void GetOneByExistingId()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void NotGetOneByNonExistingId()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void DeleteOneWithExistingId()
+        {
+            throw new NotImplementedException();
+        }
+        [Fact]
+        public void NotDeleteOneWIthNonExistingId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
