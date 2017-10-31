003ca6da15d9d111649258b47404dde8a252014e
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-10-31 10:21:19 +0100

    Initial commit

---
+++ PrayerBookDAL/Context/PrayerBookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrayerBookDAL.Context
+{
+    public class PrayerBookContext : DbContext
+    {
+        private static readonly DbContextOptions<PrayerBookContext> options =
+            new DbContextOptionsBuilder<PrayerBookContext>().UseInMemoryDatabase("TheDB").Options;
+
+        public PrayerBookContext() : base(options)
+        {
+            
+        }
+    }
+}
